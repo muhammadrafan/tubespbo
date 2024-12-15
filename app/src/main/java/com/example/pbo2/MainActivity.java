32b014c738fa7e87b301d68ef6f382760ef4134e
Rafan <rafan007@gmail.com>
2024-12-15 16:03:44 +0700

    Deskripsi perubahan yang dilakukan

--- app/src/main/java/com/example/pbo2/MainActivity.java
+++ app/src/main/java/com/example/pbo2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pbo2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_register1);
+        TextInputLayout  textinputrole = findViewById(R.id.inputlayout);
+        MaterialAutoCompleteTextView autoCompleteTextView = findViewById(R.id.inputTv);
+        Button button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (autoCompleteTextView.getText().toString().isEmpty()){
+                    textinputrole.setError("Silakan Pilih");
+                }else{
+                    Toast.makeText(MainActivity.this,autoCompleteTextView.getText().toString(),Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
